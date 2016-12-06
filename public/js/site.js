f5802b3d6b9adff08fee5e80fbcfd160dab3980d
SARISCJ <saracastillo0@gmail.com>
2016-12-06 18:03:22 -0500

    cambios https

---
+++ public/js/site.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+$(function(){
+	
+	$('[data-toggle="tooltip"]').tooltip({
+		placement : 'bottom'
+	});
+	$('.glyphicon').tooltip({
+		placement : 'bottom'
+	});
+
+});
+
+function tooltip()
+{
+	$('.glyphicon').tooltip({
+		placement : 'bottom'
+	});
+}
+
